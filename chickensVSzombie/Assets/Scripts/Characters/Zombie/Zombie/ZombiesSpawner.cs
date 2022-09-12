43b73e56f78c5ddafe501e5eeb463ca72b7041eb
facundo <facuwilli@gmail.com>
2022-09-12 11:56:31 -0300

    Reset zombie spawner on end game.
    All zombies instances in the scene will be deleted on end game.

--- chickensVSzombie/Assets/Scripts/Characters/Zombie/Zombie/ZombiesSpawner.cs
+++ chickensVSzombie/Assets/Scripts/Characters/Zombie/Zombie/ZombiesSpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using ChickenVSZombies.GameplayItems;
 
 namespace ChickenVSZombies.Characters.Zombies 
 {
@@ -12,13 +13,17 @@ namespace ChickenVSZombies.Characters.Zombies
         
         [SerializeField] private float _maxSpawnTime;
 
-        //[SerializeField] private float _maxZombiesSpawnInASingleRow;
-
-        private float _newZombieSpawnTimer;
+        [SerializeField] private short _initialZombieMaxInstances;
+        
+        private ZombieHealth[] _zombieInstances;
+        
+        private float _newZombieSpawnTimer;        
 
         void Start()
         {
-            SetRandomSpawnTimer();            
+            SetRandomSpawnTimer();
+            
+            _zombieInstances = new ZombieHealth[_initialZombieMaxInstances];            
         }
 
         void Update()
@@ -28,6 +33,34 @@ namespace ChickenVSZombies.Characters.Zombies
             InstanciateZombieIfTimerReachedZero();
         }
 
+        void OnEnable()
+        {
+            GameplayResetter.OnGameplayResset += ResetZombiesSpawner;
+        }
+
+        void OnDisable()
+        {
+            GameplayResetter.OnGameplayResset += ResetZombiesSpawner;
+        }
+
+        private void ResetZombiesSpawner() 
+        {
+            SetRandomSpawnTimer();
+
+            DestroyZombiesInScene();
+        }
+
+        private void DestroyZombiesInScene() 
+        {
+            for (short i = 0; i < _zombieInstances.Length; i++) 
+            {
+                if (_zombieInstances[i] != null) 
+                {
+                    _zombieInstances[i].DestroyZombie();
+                }
+            }
+        }
+
         private void SetRandomSpawnTimer() 
         {
             _newZombieSpawnTimer = Random.Range(_minSpawnTime, _maxSpawnTime); 
@@ -35,7 +68,7 @@ namespace ChickenVSZombies.Characters.Zombies
 
         private void DecreaseNewZombieSpawnTimer() 
         {
-            if (_newZombieSpawnTimer > 0f) 
+            if (_newZombieSpawnTimer > 0f && AreLessZombiesThanAllowed()) 
             {
                 _newZombieSpawnTimer -= Time.deltaTime;
 
@@ -65,9 +98,29 @@ namespace ChickenVSZombies.Characters.Zombies
             return _spawnPoints[random].transform.position;
         }
 
-        private void InstanciateZombie(Vector3 position) 
+        private void InstanciateZombie(Vector3 position) // 
         {
-            Instantiate(_zombiePrefab, position, Quaternion.identity);
-        }       
+            _zombieInstances[GetZombieInstancesNullIndex()] = Instantiate(_zombiePrefab, position, Quaternion.identity).GetComponent<ZombieHealth>();
+        }
+        
+        private short  GetZombieInstancesNullIndex() 
+        {
+            short index = 0;
+
+            for (short i = 0; i < _zombieInstances.Length; i++) 
+            {
+                if (_zombieInstances[i] == null) 
+                {
+                    return i;
+                }
+            }
+
+            return index;
+        }
+
+        private bool AreLessZombiesThanAllowed() 
+        {
+            return ZombieHealth.ZombiesInstances < _initialZombieMaxInstances;
+        }
     }
 }
